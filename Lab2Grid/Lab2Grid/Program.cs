e5e2f83743b00735ae9f5af2168b43b6bd74b5cd
pontiff <pekal2001@bk.ru>
2023-04-19 23:35:47 +0700

    fix_project

--- Lab2Grid/Lab2Grid/Program.cs
+++ Lab2Grid/Lab2Grid/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число а:");
+            Console.WriteLine("Введите число b:");
+            Console.WriteLine("1. Сумма");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+
             double? a = Convert.ToDouble(Console.ReadLine());
             double? b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Результат: ");
