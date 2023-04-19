5687d22302179f5368578714116a39bff5edfd6a
pontiff <pekal2001@bk.ru>
2023-04-19 22:40:16 +0700

    case

--- Lab2Grid/Lab2Grid/Program1.cs
+++ Lab2Grid/Lab2Grid/Program1.cs
@@ -4,12 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число а:");
-            Console.WriteLine("Введите число b:");
-            Console.WriteLine("1. Сумма");
-            Console.WriteLine("2. Вычитание");
-            Console.WriteLine("3. Умножение");
-            Console.WriteLine("4. Деление");
+            int? c = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Результат: ");
+            switch (c)
+            {
+                case 1: plus(a, b); break;
+                case 2: minus(a, b); break;
+                case 3: multiply(a, b); break;
+                case 4: divide(a, b); break;
+            }
         }
     }
 }
