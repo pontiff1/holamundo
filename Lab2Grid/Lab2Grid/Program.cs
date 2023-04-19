9951724080f45c0bac605abf185bc86b01f64013
pontiff <pekal2001@bk.ru>
2023-04-19 21:59:07 +0700

    Menu

---
+++ Lab2Grid/Lab2Grid/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab2Git
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число а:");
+            Console.WriteLine("Введите число b:");
+            Console.WriteLine("1. Сумма");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+        }
+    }
+}
